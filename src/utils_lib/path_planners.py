220173b0ec44b8921762a998caea4ab92c6bcfb8
KhAlamdar11 <phoenixdealamdar2@gmail.com>
2023-05-13 23:34:20 +0200

    complete refactoring

--- src/utils_lib/path_planners.py
+++ src/utils_lib/path_planners.py
@@ -1,13 +1,12 @@
 import numpy as np
 import math
-from ompl import base as ob
-from ompl import geometric as og
 
 from utils_lib.path_planning_algorithms.fmt import *
 from utils_lib.path_planning_algorithms.batch_informed_trees import BITStar
 from utils_lib.path_planning_algorithms.dubins_path import dupin_smooth
 from utils_lib.path_planning_algorithms.bspline import *
 from utils_lib.path_planning_algorithms.clean_informed import IRrtStar
+from utils_lib.path_planning_algorithms.rrt_star_ompl import RRTStarOMPL
 
 # function that choses the planner based on the inputs
 def compute_path(planner_config,start_p, goal_p, state_validity_checker, dominion, max_time=2.0):    
@@ -93,55 +92,6 @@ def x_y_to_xy(path_x,path_y):
     for i in range(len(path_x)):
         path.append((path_x[i], path_y[i]))
     return path
-
-def RRTStarOMPL(start_p, goal_p, state_validity_checker, dominion, max_time=2.0):
-    space = ob.RealVectorStateSpace(2)
-    bound_low=    dominion[0]
-    bound_high= dominion[1]
-    space.setBounds(bound_low, bound_high)
-    space.setLongestValidSegmentFraction(0.001)
-    si = ob.SpaceInformation(space) 
-    si.setStateValidityChecker(ob.StateValidityCheckerFn(state_validity_checker))
-        # create a start state
-    start = ob.State(space)
-    start[0] = start_p[0]
-    start[1] = start_p[1]
-    
-    # create a goal state
-    goal = ob.State(space)
-    goal[0] = goal_p[0]
-    goal[1] = goal_p[1]
-
-    pdef = ob.ProblemDefinition(si)
-    pdef.setStartAndGoalStates(start, goal)
-    pdef.setOptimizationObjective(ob.PathLengthOptimizationObjective(si))
-    optimizingPlanner = og.RRTstar(si)
-    optimizingPlanner.setRange(10)  
-    optimizingPlanner.setGoalBias(0.2)
-
-    # Set the problem instance for our planner to solve and call setup
-    optimizingPlanner.setProblemDefinition(pdef)
-    optimizingPlanner.setup()
-
-    # attempt to solve the planning problem in the given runtime
-    solved = optimizingPlanner.solve(max_time)
-    
-    # Get planner data
-    pd = ob.PlannerData(si)
-    optimizingPlanner.getPlannerData(pd)
-    
-    if solved:
-        # get the path and transform it to a list
-        path = pdef.getSolutionPath()
-        print("Found solution:\n%s" % path)
-        ret = []
-        for i in path.getStates():
-            ret.append((i[0], i[1]))
-    else:
-        ret = []
-        print("No solution found")
-    # print ("path", path)
-    return ret
         
 def Add_theta(path_x,path_y):
     path = []
